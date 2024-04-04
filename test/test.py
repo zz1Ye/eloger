9b9f326b67b8dcf7878f55d904f4c4a3eb76735a
zzYe <zzy_nic@126.com>
2024-04-04 18:34:36 +0800

    update example

--- test/test.py
+++ test/test.py
@@ -6,17 +6,17 @@
 @Author  : zzYe
 
 """
-from config.config import load_config
-from digger import Parser
+from conf.core import Config
+from core import Parser
 
 if __name__ == '__main__':
-    config = load_config()
+    config = Config()
 
     # Parsing event logs
     # ETH Test --- Parsing event logs
     tx_hash = "0x2f13d202c301c8c1787469310a2671c8b57837eb7a8a768df857cbc7b3ea32d8"
     parser = Parser("ETH", config)
-    elogs = parser.get_event_logs(tx_hash)
+    elogs = parser.parse_event_logs(tx_hash)
 
     for elog in elogs:
         print(elog)
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     # BSC Test --- Parsing event logs
     tx_hash = "0xe82d9c4362cede63f93e381700ff01b8dd28c3de2eec4b2f077b3dc2beb4f088"
     digger = Parser("BSC", config)
-    elogs = digger.get_event_logs(tx_hash)
+    elogs = digger.parse_event_logs(tx_hash)
 
     for elog in elogs:
         print(elog)
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     # Polygon Test --- Parsing event logs
     tx_hash = "0x92641e15f1f7ed839072e015369886f17fcd4ca31ad08f75c87d130369b6b1b5"
     digger = Parser("POL", config)
-    elogs = digger.get_event_logs(tx_hash)
+    elogs = digger.parse_event_logs(tx_hash)
 
     for elog in elogs:
         print(elog)
